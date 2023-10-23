ee08e746a44d00bda4efc8a59d96028c610ac185
kempny <benjamin.kempny@student-reutlingen-university.degit config --global user.name Benjamin>
2023-10-23 14:29:32 +0200

    Commit Test

---
+++ components/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Bild from '../public/Bild.jpg'
+const Image = () => {
+    return (
+        <img src={Bild} alt={"Bild"} />
+    );
+};
+
+export default Image;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import React from "react";
 import styles from '../styles/styles.module.css'
+import Image from '../components/Image.js'
 function Home() {
         return (
             <div className={styles.parallaxOuter}>
-                <div className={styles.parallax}></div>
-                <img src={}/>
+                <Image></Image>
             </div>
         );
 }
