067f4726a79d5404e2e7c7e8c375e605c86c88c6
Erik Berner <erik.berner@reutlingen-university.de>
2023-11-13 18:29:50 +0100

    Changed Pictures for the second page and added linear-gradient background, finished landing page, cleanup of unused material

--- components/OurFuture.js
+++ components/OurFuture.js
@@ -6,18 +6,18 @@ import Lenis from '@studio-freight/lenis'
 import { useTransform, useScroll, motion } from 'framer-motion';
 
 const images = [
-    "/1.jpg",
-    "/2.jpg",
-    "/3.jpg",
-    "/4.jpg",
-    "/5.jpg",
-    "/6.jpg",
-    "/7.jpg",
-    "/8.jpg",
-    "/9.jpg",
-    "/10.jpg",
-    "/11.jpg",
-    "/12.jpg",
+    "/Abholzung.jpg",
+    "/Waldbrand2.jpg",
+    "/Waldbrand.jpg",
+    "/Eisberg.jpg",
+    "/Eisbär.jpg",
+    "/Flut.jpg",
+    "/Müllberg.jpg",
+    "/One World.jpg",
+    "/SchmelzenDesPolareises.jpg",
+    "/Smog.jpg",
+    "/Tornado.jpg",
+    "/Dürre.jpg",
 ]
 
 export default function Home() {
